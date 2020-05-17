6a6db8d54bffaf20adaca1d06208f8fb0896681c
Rameez CM <rameezcm88@gmail.com>
2020-05-17 14:26:10 +0200

    Update SSRController.java

--- src/main/java/com/rameez/rameezserversiderendering/controller/SSRController.java
+++ src/main/java/com/rameez/rameezserversiderendering/controller/SSRController.java
@@ -3,9 +3,9 @@ package com.rameez.rameezserversiderendering.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class SSRController {
 
 	@RequestMapping("/")
