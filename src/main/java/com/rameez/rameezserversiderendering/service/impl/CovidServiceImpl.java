810f3912fb37b97c44a492477411a14b791fcb56
Rameez CM <rameezcm88@gmail.com>
2020-05-17 19:27:41 +0200

    Update CovidServiceImpl.java

--- src/main/java/com/rameez/rameezserversiderendering/service/impl/CovidServiceImpl.java
+++ src/main/java/com/rameez/rameezserversiderendering/service/impl/CovidServiceImpl.java
@@ -1,13 +1,49 @@
 package com.rameez.rameezserversiderendering.service.impl;
 
 import com.rameez.rameezserversiderendering.model.CovidModel;
+import com.rameez.rameezserversiderendering.model.Example;
 import com.rameez.rameezserversiderendering.service.CovidService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CovidServiceImpl implements CovidService{
 
+	@Value("${single.country.api}")
+	private String singleCountry ;
+	@Value("${all.country.api}")
+	private String allCountries;
+
 	@Override
-	public CovidModel getCovidCount(String county) {
-		return null;
+	public List<Mono<Example>> getCovidCount(String county) {
+		List<Mono<Example>> result = new ArrayList<>();
+		result.add(getCount(county));
+		return result;
+	}
+
+	@Override
+	public Flux<CovidModel> getAllCountries() {
+		WebClient allCountryclient = WebClient.create(allCountries);
+		Flux<CovidModel> flux = allCountryclient.get()
+				.uri("count")
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToFlux(CovidModel.class);
+		return flux;
+	}
+
+	Mono<Example> getCount(String country) {
+		WebClient singleCountryclient = WebClient.create(singleCountry);
+		return singleCountryclient.get().uri(country).accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.flatMap(clientResponse -> clientResponse.bodyToMono(Example.class));
 	}
 
 }
