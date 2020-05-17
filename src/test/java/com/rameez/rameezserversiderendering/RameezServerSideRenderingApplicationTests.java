e2f2a58a1d187383632c87a474501ee2d5793261
Rameez CM <rameezcm88@gmail.com>
2020-05-17 23:46:48 +0200

    Update RameezServerSideRenderingApplicationTests.java

--- src/test/java/com/rameez/rameezserversiderendering/RameezServerSideRenderingApplicationTests.java
+++ src/test/java/com/rameez/rameezserversiderendering/RameezServerSideRenderingApplicationTests.java
@@ -1,13 +1,12 @@
 package com.rameez.rameezserversiderendering;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RameezServerSideRenderingApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
