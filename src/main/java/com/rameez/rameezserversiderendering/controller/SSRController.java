f164f4556dd884442f787dd81fb0ba9565a46ddb
Rameez CM <rameezcm88@gmail.com>
2020-05-17 19:28:09 +0200

    Update SSRController.java

--- src/main/java/com/rameez/rameezserversiderendering/controller/SSRController.java
+++ src/main/java/com/rameez/rameezserversiderendering/controller/SSRController.java
@@ -1,20 +1,40 @@
 package com.rameez.rameezserversiderendering.controller;
 
+import com.rameez.rameezserversiderendering.model.Example;
+import com.rameez.rameezserversiderendering.service.CovidService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Controller
 public class SSRController {
 
+	@Autowired
+	private CovidService covidService;
+
 	@RequestMapping("/")
 	public String welcomePage(final Model model) {
+		int elementsOfFluxInSseChunk = 1;
+		List<Mono<Example>> covidList = covidService.getCovidCount("USA");
+		model.addAttribute("covid", new ReactiveDataDriverContextVariable(
+				Flux.concat(covidList),
+				elementsOfFluxInSseChunk));
 		return "welcome";
 	}
 
 	@RequestMapping("/rameez/count/{county}")
 	public String getCovidCountForCountry(final Model model ,@PathVariable(name = "country") String county) {
-		return "index";
+		int elementsOfFluxInSseChunk = 1;
+		model.addAttribute("covid", new ReactiveDataDriverContextVariable(
+				Flux.concat(covidService.getCovidCount(county)),
+				elementsOfFluxInSseChunk));
+		return "welcome";
 	}
 }
