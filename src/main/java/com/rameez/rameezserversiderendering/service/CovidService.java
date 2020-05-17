4498e63cf76331119dce199df1311dc31f2b2c8d
Rameez CM <rameezcm88@gmail.com>
2020-05-17 23:45:33 +0200

    Update CovidService.java

--- src/main/java/com/rameez/rameezserversiderendering/service/CovidService.java
+++ src/main/java/com/rameez/rameezserversiderendering/service/CovidService.java
@@ -1,13 +1,12 @@
 package com.rameez.rameezserversiderendering.service;
 
 import com.rameez.rameezserversiderendering.model.CovidModel;
-import com.rameez.rameezserversiderendering.model.Example;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface CovidService {
-	List<Mono<Example>> getCovidCount(String county);
+	List<Mono<CovidModel>> getCovidCount(String county);
 	 Flux<CovidModel> getAllCountries();
 }
